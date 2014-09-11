44a5c85ea471ff90fda02ada0ebff5778e50bc72
Mike <mikegrnewald@yahoo.de>
2014-09-12 00:36:11 +0200

    Init Projekt
    Init Projekt

---
+++ JimaineRolePlayWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JimaineRolePlayWebsite.Startup))]
+namespace JimaineRolePlayWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
